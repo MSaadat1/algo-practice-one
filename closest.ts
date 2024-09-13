63a2953c79cc45d9866afa0ade36f62fc3a0c899
Mezhgan Saadat <mezhgan@mezhgans-air.mynetworksettings.com>
2024-09-13 16:10:29 -0400

    first commit

--- closest.ts
+++ closest.ts
@@ -1,6 +1,18 @@
 export function closest(nums: number[], target: number): number {
    // Implement me
-   return 0;
+   let closetSums = Infinity;
+   for(let i = 0; i < nums.length - 2; i ++){
+    for(let j = i + 1; j <nums.length - 1; j++){
+      for(let k = j + 1; k < nums.length; k++){
+        let sums = nums[i] + nums [j] + nums[k];
+        if(Math.abs(sums - target) < (Math.abs(closetSums - target))){
+          closetSums = sums
+        }
+      }
+    }
+   }
+   
+   return closetSums;
 }
 
 module.exports = { closest }
